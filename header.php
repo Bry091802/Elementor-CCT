d38b4414b4eec303f05b56c648d8d3ace2578dd3
bullets012 <lancegenesismercado0111@gmail.com>
2024-11-13 07:18:27 +0800

    CCT

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CCT International</title>
+    <?php wp_head() ?>
+</head>
+<body>
+    
+
+<header class="header">
+<div class="container">
+    <div class="wrapper">
+        <div class="logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="">
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Courses</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+</div>
+</header>
